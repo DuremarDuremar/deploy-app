ca2dad4b6c824e85d698e4e2a74a979f8df8a174
DuremarDuremar <duremar98@yandex,ru>
2020-09-04 00:52:32 +0300

    Updates

---
+++ precache-manifest.e72edf88b1afe5f1b6670f82c6af8e33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c08d8b5f4653cd89de2d52416f2a0e",
+    "url": "/deploy-app/index.html"
+  },
+  {
+    "revision": "b2541e7c91a14f98454e",
+    "url": "/deploy-app/static/css/main.a79c7a6c.chunk.css"
+  },
+  {
+    "revision": "9840abe94cc7accc3b2e",
+    "url": "/deploy-app/static/js/2.c436efef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-app/static/js/2.c436efef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2541e7c91a14f98454e",
+    "url": "/deploy-app/static/js/main.1cb1affd.chunk.js"
+  },
+  {
+    "revision": "04724fc3230f20dc116b",
+    "url": "/deploy-app/static/js/runtime-main.8d774ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
